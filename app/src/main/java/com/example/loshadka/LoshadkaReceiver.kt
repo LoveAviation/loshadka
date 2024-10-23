ebcb239fef3e7233d1af430e1beebba68ba11c6e
LoveAviation <ivangeorgmarcov@gmail.com>
2024-10-23 19:36:22 +0300

    ready?

--- app/src/main/java/com/example/loshadka/LoshadkaReceiver.kt
+++ app/src/main/java/com/example/loshadka/LoshadkaReceiver.kt
@@ -3,25 +3,38 @@ package com.example.loshadka
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 
 class LoshadkaReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val message = intent?.getStringExtra("message")
-        if (message == "Loshadka!") {
 
-            if (context is MainActivity) {
-                context.incrementMessageCount()
+        val handler = Handler(Looper.getMainLooper())
+        val result = goAsync()
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                val message = intent?.getStringExtra("message")
+                if (message == "Loshadka!") {
+
+                    if (context is MainActivity) {
+                        handler.post{
+                            context.incrementMessageCount()
+                        }
+                    }
+
+
+                    sleep(5000)
+
+                    val intentBack = Intent("com.example.SECRET_MESSAGE").apply {
+                        putExtra("message", "Yozhik!")
+                        setPackage("com.example.yozhik")
+                    }
+                    context?.sendBroadcast(intentBack)
+                    result.finish()
+                }
             }
-
-
-            Thread.sleep(5000)
-
-            val intentBack = Intent("com.example.SECRET_MESSAGE").apply {
-                putExtra("message", "Yozhik!")
-                setPackage("com.example.yozhik")
-            }
-            context?.sendBroadcast(intentBack)
         }
+        thread.start()
     }
 }
